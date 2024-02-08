774d8d8defb33f1fe7ce05d7e1867efb1b1d1705
Talita1996 <talita_mota@outlook.com>
2024-02-08 18:59:04 -0300

    página 404 adicionada

--- global-templates/404/404.php
+++ global-templates/404/404.php
@@ -1,3 +1,8 @@
-<div class="wrap container">
-
+<div class="wrap container text-center">
+    <section id="not-found-section" class="container-fluid py-4">
+        <span>404</span>
+        <h1 class="mb-3">Página não encontrada</h1>
+        <a class="btn btn-primary" href="<?php echo home_url(); ?>">Voltar para a home</a>
+        
+    </section>
 </div>
